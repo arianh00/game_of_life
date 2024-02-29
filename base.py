e872f8f8e917333788d5d764974475289558635f
arianh00 <arianh00@estudiantes.unileon.es>
2024-02-29 13:59:33 +0100

    Added cursor functionality.

--- base.py
+++ base.py
@@ -5,17 +5,32 @@ ALIVE_COLOR = '#f3faf2'
 DEAD_COLOR = '#292b29'
 LINES_COLOR = '#969e96'
 CELL_SIZE = 20
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 800
 
 # Global variables
 simulation_state = False
+grid = [[0 for i in range(WINDOW_WIDTH//CELL_SIZE)] for j in range(WINDOW_HEIGHT//CELL_SIZE)]
 
-# Functions
+# Interacion Functions
+def cell_drawer(x, y, status):
+    if status == 1:
+        color = ALIVE_COLOR
+    else:
+        color = DEAD_COLOR
+    canvas.create_rectangle(x * CELL_SIZE, y * CELL_SIZE, (x + 1) * CELL_SIZE, (y + 1) * CELL_SIZE, outline=LINES_COLOR, fill=color)
+
 def left_click(event):
     if not simulation_state:
         x, y = event.x, event.y
-        x = x // CELL_SIZE * CELL_SIZE
-        y = y // CELL_SIZE * CELL_SIZE
-        canvas.create_rectangle(x, y, x + CELL_SIZE, y + CELL_SIZE, outline=LINES_COLOR, fill=ALIVE_COLOR)
+        x = x // CELL_SIZE
+        y = y // CELL_SIZE
+        if grid[y][x] == 0:
+            grid[y][x] = 1
+            cell_drawer(x, y, 1)
+        else:
+            grid[y][x] = 0
+            cell_drawer(x, y, 0)
 
 def start_stop_simulation(event):
     if event.char == 'r' and not simulation_state:
@@ -27,6 +42,20 @@ def start_stop_simulation(event):
         print('Simulation stopped')
         #stop simulation
 
+def clear_grid(event):
+    if not simulation_state:
+        for y, row in enumerate(grid):
+            for x, status in enumerate(row):
+                grid[y][x] = 0
+        draw_grid()
+
+def draw_grid():
+    for y, row in enumerate(grid):
+        for x, status in enumerate(row):
+            cell_drawer(x, y, status)
+
+# Logic Functions
+
 # Create the root window
 root = tk.Tk()
 root.title('Base')
@@ -37,9 +66,7 @@ canvas = tk.Canvas(root, bg=DEAD_COLOR)
 canvas.pack(expand=True, fill='both')
 
 # Draw the grid
-for i in range(0, 1000, CELL_SIZE):
-    canvas.create_line(i, 0, i, 800, fill=LINES_COLOR)
-    canvas.create_line(0, i, 1000, i, fill=LINES_COLOR)
+draw_grid()
 
 root.bind('<Button-1>', left_click)
 root.bind('<Key>', start_stop_simulation)
